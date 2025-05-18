0be1c4e2627fe7355fd2b407f5fa7a95390b1797
bkasman95 <bkasman95@gmail.com>
2025-05-17 22:15:56 -0400

    Validation: show error for invalid chord input

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 from key_detector import KeyDetector
+import os
 
 app = Flask(__name__)
 detector = KeyDetector()
+
+# Define valid chords (should match frontend options)
+VALID_CHORDS = set([
+    # Major, minor, dim
+    *(root + t for root in ["C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B"] for t in ["", "m", "dim"]),
+    # mb5, m-5
+    *(root + "mb5" for root in ["C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B"]),
+    *(root + "m-5" for root in ["C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B"]),
+])
 
 @app.route('/')
 def home():
@@ -12,14 +22,17 @@ def home():
 def analyze():
     data = request.get_json()
     chord_progression = data.get('chords', '').split()
-    
+    print('Received chords:', chord_progression)
     # Clean the chords
     cleaned_chords = []
     for chord in chord_progression:
         clean_chord = chord.strip().replace(',', '').replace("'", '').replace('"', '')
         if clean_chord:
             cleaned_chords.append(clean_chord)
-    
+    print('Cleaned chords:', cleaned_chords)
+    # Validate chords
+    if not cleaned_chords or not all(chord in VALID_CHORDS for chord in cleaned_chords):
+        return jsonify({'error': 'Please enter a valid chord.'})
     # Find possible keys
     possible_keys = detector.find_possible_keys(cleaned_chords)
     
@@ -34,5 +47,7 @@ def analyze():
     
     return jsonify({'results': results})
 
+# For local development
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
